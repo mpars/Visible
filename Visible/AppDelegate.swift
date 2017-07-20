f3d74b3bfd848b3ff109a7b908cb87224e7819ed
Mark Parsons <mark@alphabetthreat.co.uk>
2017-07-20 12:59:31 +0100

    Initial Commit

---
+++ Visible/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Visible
+//
+//  Created by mark on 20/07/2017.
+//  Copyright © 2017 mpars. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
